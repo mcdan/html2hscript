1f94413e238d4f47c4d4cdb04ea255b65eea3fee
mcdan <mcweeney@adobe.com>
2015-04-07 13:22:09 -0400

    Change to use a virt-dom attributes hash

--- test/index_test.js
+++ test/index_test.js
@@ -3,7 +3,7 @@ var parser = require('../');
 
 test('should return hyperscript', function(t) {
   parser('<h1 foo="beep">Hello World</h1>', function(err, hscript) {
-    t.equals(hscript, 'h("h1", { "foo": "beep"}, [ "Hello World" ])', 'success')
+    t.equals(hscript, 'h("h1", {"attributes" : {"foo": "beep"}}, [ "Hello World" ])', 'success')
     t.end()
   })
 })
